403a2df4834a5690198e58952decee5e4aa02b82
nextgentechth <sugasame.th@gmail.com>
2026-06-28 11:05:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const AppConfig = {
+  LIFF_ID: "2010395332-urAIJlTe", // ใส่ LIFF ID จริงที่นี่
+  GAS_URL: "https://script.google.com/macros/s/AKfycbyOBXix2UwTXGMn4H-rvvof_jj51rg1elvGYViPZjXo1-Yx42kIVnOWXpkRqyY0xBQs/exec" // ใส่ URL จริงที่นี่
+};
