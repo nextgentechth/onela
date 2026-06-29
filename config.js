4b5aa20147042abad83342418b2f2dca6d41ab55
nextgentechth <sugasame.th@gmail.com>
2026-06-29 21:04:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const AppConfig = {
   LIFF_ID: "2010395332-urAIJlTe", // ใส่ LIFF ID จริงที่นี่
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyOBXix2UwTXGMn4H-rvvof_jj51rg1elvGYViPZjXo1-Yx42kIVnOWXpkRqyY0xBQs/exec" // ใส่ URL จริงที่นี่
+  GAS_URL: "https://script.google.com/macros/s/AKfycbx3x5c1054TK25Ap8359xNr1Z77e9ic_Iz5e3qgnZKWZBcLKyGC0PODnqcJbdAWk7mg/exec" // ใส่ URL จริงที่นี่
 };
